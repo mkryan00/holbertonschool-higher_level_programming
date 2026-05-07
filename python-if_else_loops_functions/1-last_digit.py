3e7f9e41352e6ec52ca77f62d57ce5f0cdb6a825
mkryan00 <13073@holbertonstudents.com>
2026-05-07 06:03:26 +0000

    Added space before 'and' to join strings correctly

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ last_digit = number % 10 if number > 0 else (number % -10)
 print("Last digit of", f"{number}", "is", f"{last_digit}", end="")
 
 if last_digit > 5:
-    print("and is greater than 5")
+    print(" and is greater than 5")
 
 if last_digit == 0:
-    print("and is 0")
+    print(" and is 0")
 
 if last_digit < 6 and last_digit != 0:
-    print("and is less than 6 and not 0")
+    print(" and is less than 6 and not 0")
