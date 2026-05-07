054d96c2412529cbee1f6600912983a034c1239c
mkryan00 <13073@holbertonstudents.com>
2026-05-07 06:00:25 +0000

    Modified file. Shortened line length. Rearranged print statements. Added optional parameter end=

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10 if number > 0 else (number % -10) 
+last_digit = number % 10 if number > 0 else (number % -10)
+
+print("Last digit of", f"{number}", "is", f"{last_digit}", end="")
 
 if last_digit > 5:
-    print("Last digit of", f"{number}", "is", f"{last_digit}", "and is greater than 5")
+    print("and is greater than 5")
 
 if last_digit == 0:
-    print("Last digit of", f"{number}", "is", f"{last_digit}", "and is 0")
+    print("and is 0")
 
 if last_digit < 6 and last_digit != 0:
-    print("Last digit of", f"{number}", "is", f"{last_digit}", "and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
