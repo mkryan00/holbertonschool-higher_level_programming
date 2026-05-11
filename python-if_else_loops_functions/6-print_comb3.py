68b5acb51a39576dd1e1785e659bf1514664e298
mkryan00 <13073@holbertonstudents.com>
2026-05-11 01:03:27 +0000

    Fixed pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(10):
         if (i != j and i < j) and i < 9:
             if (i == 8 and j == 9):
-                print("{0}{1}".format(i,j))
+                print("{0}{1}".format(i, j))
             else:
-                print("{0}{1}, ".format(i,j), end="")
+                print("{0}{1}, ".format(i, j), end="")
