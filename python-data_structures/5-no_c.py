cc972f696df1f7f84a643bc0d6ab2d1ccf0b4a65
mkryan00 <13073@holbertonstudents.com>
2026-05-14 00:55:52 +0000

    Task 5. Can you C me now?

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string = new_string + char
+    return new_string
