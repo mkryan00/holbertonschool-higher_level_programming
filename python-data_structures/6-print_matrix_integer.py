8e5c0697afeea44bfde2e4153297a6a290e4e165
mkryan00 <13073@holbertonstudents.com>
2026-05-14 01:54:27 +0000

    Correcte pycodestyle - indent

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-            print(' '.join('{:d}'.format(element)for element in row))
+        print(' '.join('{:d}'.format(element)for element in row))
