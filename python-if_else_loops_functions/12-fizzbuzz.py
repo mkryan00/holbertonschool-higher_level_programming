667e88f475de6f8bde55eef5e0e13d215dfb3e0d
mkryan00 <13073@holbertonstudents.com>
2026-05-19 07:29:19 +0000

    Fixed output - restructured if,elif

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(1, 100):
+    for num in range(1, 101):
         if num % 5 == 0 and num % 3 == 0:
-            print(f'FizzBuzz', end=" ")
-        if num % 5 == 0:
-            print(f'Buzz', end=" ")
-        if num % 3 == 0:
-            print(f'Fizz', end=" ")
+            print('{:s}'.format('FizzBuzz'), end=" ")
+        elif num % 3 == 0:
+            print('{:s}'.format('Fizz'), end=" ")
+        elif num % 5 == 0:
+            print('{:s}'.format('Buzz'), end=" ")
         else:
             print('{:d}'.format(num), end=" ")
