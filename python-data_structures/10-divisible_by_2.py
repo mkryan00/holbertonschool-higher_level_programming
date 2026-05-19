3442ee38f90e8c827ef00032bf585c24df73ca5c
mkryan00 <13073@holbertonstudents.com>
2026-05-19 23:32:20 +0000

    Added task 10. Only by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+
+    results = []
+    for n in my_list:
+        if n % 2 == 0:
+            results.append(True)
+        else:
+            results.append(False)
+            return (results)
