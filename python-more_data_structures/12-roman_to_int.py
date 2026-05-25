f26755443ddcbe4191a21bda41a079cf5fad6807
mkryan00 <13073@holbertonstudents.com>
2026-05-25 03:08:39 +0000

    Added task 12. Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,8 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
               'C': 100, 'D': 500, 'M': 1000}
-    
     result = 0
     for i in range(len(roman_string)):
         current = romans[roman_string[i]]
