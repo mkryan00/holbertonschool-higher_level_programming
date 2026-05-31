8babf8a689846261392c9c4a3dcaf525286a6367
mkryan00 <13073@holbertonstudents.com>
2026-05-31 07:32:16 +0000

    Fixed pycodestyle, comment lines too long

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     res = None
-    # initialise res = None - when b = 0. Without it, the except block would set nothing and finally would try to print res which wouldn't be assigned (NameError).
+    # initialise res = None - when b = 0.
+    # Without it, the except block would set nothing.
+    # Finally would try to print res which hasn't be assigned.
+    # (NameError)
     try:
         res = a/b
     except ZeroDivisionError:
