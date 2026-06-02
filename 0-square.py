ba60ee4516e0949712c31c903d238eb7a078126b
mkryan00 <13073@holbertonstudents.com>
2026-06-02 11:18:48 +0000

    Added task 0. My first square

---
+++ 0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for a Square class.
+"""
+
+
+class Square:
+    # Define a new classed called Square
+    # This is the blueprint for creating Square objects.
+    pass
+# Tells python "this block is intentionally empty".
+# Without it, Python would throw an error because
+# a class body can't be completely empty.
+# When Square() is run in test file,
+# Python creates a new instance of the Square class;
+# an actual object built from that blueprint.
