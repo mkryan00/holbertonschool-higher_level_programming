09c5d61e4cbad26fb7a16709bebf78b8dd22429b
mkryan00 <13073@holbertonstudents.com>
2026-06-11 10:05:43 +0000

    Added task 2. Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Defines a Square class.
+"""
+
+
+class Square:
+    
+    def __init__(self, size=0):
+        """
+        The __init__ method initializes the size value of the square.
+
+        Attribute:
+        size (int): The size of the square. Defaults to 0.
+
+        Raises:
+        TypeError: If the size is not an integer.
+        ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
