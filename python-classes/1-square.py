7706d3690c72cc32128a49eb5ce70dc7c403bcc8
mkryan00 <13073@holbertonstudents.com>
2026-06-11 09:50:59 +0000

    Added task 1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines a Square class.
+"""
+
+
+class Square:
+    """
+    Empty square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square.
+        """
+        self.__size = size
