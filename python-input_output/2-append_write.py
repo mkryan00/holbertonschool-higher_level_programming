b522d3520c07b7e8e5ca84d322b43ace68ae6927
mkryan00 <13073@holbertonstudents.com>
2026-06-22 05:02:35 +0000

    Added task 2. Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that appends a string to a text file
+and returns number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
