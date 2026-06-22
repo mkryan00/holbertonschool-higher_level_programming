cf9f7eac4607b9a6d2dc1124fe5de9d69895b398
mkryan00 <13073@holbertonstudents.com>
2026-06-22 06:03:57 +0000

    Fixed typo of object in docstring

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -5,5 +5,5 @@ import json
 
 
 def from_json_string(my_str):
-    """Returns an objtec represented by a JSON string"""
+    """Returns an object represented by a JSON string"""
     return json.loads(my_str)
