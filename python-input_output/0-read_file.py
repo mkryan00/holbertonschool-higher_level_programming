0ff99990d154244d7fffd4be838dcb7f49c94e29
mkryan00 <13073@holbertonstudents.com>
2026-06-22 04:21:11 +0000

    Added documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Module that defines the read_file function."""
+
+
 def read_file(filename=""):
+    """Reads a text file and prints its content to stdout.
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+    """
     with open(filename) as f:
         print(f.read())
